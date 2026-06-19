0514ddec760ff8e4485e7f575f647dfaf819bfeb
d-senja <dsenja2001@icloud.com>
2026-06-19 19:54:41 +0100

    Delete speed-insights.js

--- speed-insights.js
+++
@@ -1,6 +0,0 @@
-// Vercel Speed Insights initialization
-// This script imports and initializes @vercel/speed-insights for the static HTML site
-import { injectSpeedInsights } from './node_modules/@vercel/speed-insights/dist/index.mjs';
-
-// Initialize Speed Insights
-injectSpeedInsights();
